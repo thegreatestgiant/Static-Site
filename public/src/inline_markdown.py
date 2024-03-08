bcc06c92221bd4a3108231e7a1a67db2051b060f
Sean Alter <seanalter1@gmail.com>
2024-03-07 22:07:43 -0500

    :construction: Update to diff function

--- public/src/inline_markdown.py
+++ public/src/inline_markdown.py
@@ -29,18 +29,39 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     return re.findall(r"\[(.*?)\]\((.*?)\)", text)
 
+
 def split_nodes_image(old_nodes):
     nodes = []
     for node in old_nodes:
         images = extract_markdown_images(node.text)
-        for image_tup in images:
-            one = node.text.split(f"![{image_tup[0]}]({image_tup[1]})", 1)
-            if len(one[0]) is not 0:
-                nodes.append(TextNode(one[0], "text"))
-            nodes.append(TextNode(image_tup[0], "image", image_tup[1]))
-            if len(one) > 0 and len(one[1]) is not 0 and not (r"!\[(.*?)\]\((.*?)\)") in one[1]:
-                nodes.append(TextNode(one[1], "text"))
+        nodes.extend(recur(node.text, images, "image"))
+        # for image_tup in images:
+        #     one = node.text.split(f"![{image_tup[0]}]({image_tup[1]})", 1)
+        #     if len(one[0]) is not 0:
+        #         nodes.append(TextNode(one[0], "text"))
+        #     nodes.append(TextNode(image_tup[0], "image", image_tup[1]))
+        #     if len(one) > 0 and len(one[1]) is not 0 and not (r"!\[(.*?)\]\((.*?)\)") in one[1]:
+        #         nodes.append(TextNode(one[1], "text"))
     return nodes
+
+
+def recur(text, tup_arr, type):
+    txt, nodes, i = True, [], 0
+    arr = text.split(r"!\[(.*?)\]\((.*?)\)")
+    for str in arr:
+        if len(str) is None:
+            continue
+        if txt:
+            txt = False
+            nodes.append(TextNode(str, "text"))
+        else:
+            txt = True
+            if i >= len(tup_arr):
+                continue
+            nodes.append(TextNode(tup_arr[i][0], type, tup_arr[i][1]))
+            i += 1
+    return nodes
+
 
 def split_nodes_link(old_nodes):
     nodes = []
