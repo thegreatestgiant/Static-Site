ca8fe702492a5159f1ce7d1d7ba6dfeff6023e34
Sean Alter <seanalter1@gmail.com>
2024-03-14 10:06:50 -0400

    :white_check_mark: pass the checks

--- public/src/markdown_blocks.py
+++ public/src/markdown_blocks.py
@@ -7,9 +7,9 @@ def markdown_to_blocks(markdown):
     return blocks
 
 def block_to_block_type(block: str):
-    first = block.split("", 1)[0]
+    first = block.split(" ")[0]
     # Heading
-    if "#" in first and not len(first) > 6 and all(x == block[0] for x in block):
+    if "#" in first and not len(first) > 6 and all(x == first[0] for x in first):
         return "heading"
     # Code
     if block.startswith("```") and block.endswith("```"):
@@ -23,11 +23,11 @@ def block_to_block_type(block: str):
             return "quote"
         if "*" in firsts or "-" in firsts:
             return "unordered_list"
-        works = True
-        for i in range(len(firsts)):
-            if (i + 1) not in firsts[i]:
-                works = False
-        if works:
-            return "ordered_list"
+    works = True
+    for i in range(len(firsts)):
+        if f"{i + 1}" not in firsts[i]:
+            works = False
+    if works:
+        return "ordered_list"
     return "paragraph"
 
--- public/src/test_markdown_blocks.py
+++ public/src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -46,6 +46,20 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), "heading")
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), "code")
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), "quote")
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), "unordered_list")
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), "ordered_list")
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), "paragraph")
 
 
 if __name__ == "__main__":
