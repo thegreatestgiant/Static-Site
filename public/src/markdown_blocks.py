754a4f4a5b51a0b48c4158799c3db087b07d038d
Sean Alter <seanalter1@gmail.com>
2024-03-14 09:18:04 -0400

    :heavy_plus_sign: add block_to_block identifier

--- public/src/markdown_blocks.py
+++ public/src/markdown_blocks.py
@@ -5,3 +5,29 @@ def markdown_to_blocks(markdown):
             continue
         blocks.append(block.strip())
     return blocks
+
+def block_to_block_type(block: str):
+    first = block.split("", 1)[0]
+    # Heading
+    if "#" in first and not len(first) > 6 and all(x == block[0] for x in block):
+        return "heading"
+    # Code
+    if block.startswith("```") and block.endswith("```"):
+        return "code"
+    lines = block.splitlines()
+    firsts = []
+    for line in lines:
+        firsts.append(line.split()[0])
+    if all(x == firsts[0] for x in firsts):
+        if ">" in firsts:
+            return "quote"
+        if "*" in firsts or "-" in firsts:
+            return "unordered_list"
+        works = True
+        for i in range(len(firsts)):
+            if (i + 1) not in firsts[i]:
+                works = False
+        if works:
+            return "ordered_list"
+    return "paragraph"
+
